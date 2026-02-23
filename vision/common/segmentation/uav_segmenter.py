f81884c6d858afd3f72c50489d19f4a189709035
analieseg0809 <agonzalez@csu.fullerton.edu>
2026-02-22 19:36:25 -0800

    finished first class

--- vision/common/segmentation/uav_segmenter.py
+++ vision/common/segmentation/uav_segmenter.py
@@ -13,9 +13,21 @@ class SegmenterConfig: # we're doing green and white since the football field wi
     white_lo: Tuple[int, int, int] = (0, 0, 180)
     white_hi: Tuple[int, int, int] = (179, 60, 255)
 
-    g_kernel: int = 5
+    bg_kernel: int = 5
     obstacle_kernel: int = 7
     bg_open_iter: int = 1
     bg_close_iter: int = 2
     ob_open_iter: int = 1
     ob_close_iter: int = 2
+
+    grid_h: int = 40
+    grid_w: int = 40
+    occ_thresh: float = 0.03
+
+    process_interval_sec: float = 0.5 # this is the "still snap every .5 secs"
+
+    def _odd(k: int) -> int:
+        return k if k % 2 == 1 else k + 1
+
+
+class FieldObstacleSegmenter:
